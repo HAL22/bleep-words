b777962c26e25bd4f52d889e8fa222a28bcbfef9
Thethela Faltein <thethelafaltein@Thethelas-MacBook-Pro-2.local>
2024-06-12 15:43:19 +0200

    Getting the audio via file name

--- app.py
+++ app.py
@@ -3,17 +3,20 @@ from pydub import AudioSegment
 from pydub.playback import play
 import bleep as bl
 import ffmpeg
+from tempfile import NamedTemporaryFile
 
 st.title("Bleep words/phrases from any audio")
 
-uploaded_file = st.file_uploader("Choose an audio file",type = [".wav",".wave",".ogg",".mp3",".flac"])
+uploaded_file = st.file_uploader("Choose an audio file",type = ["wav"])
 
 text = st.text_input("Enter words or phrases you want to censor/bleep:")
 
 if uploaded_file is not None and text is not None:
-    audio = AudioSegment.from_ogg(uploaded_file)
+    with NamedTemporaryFile(suffix="wav") as temp:
+        temp.write(uploaded_file.getvalue())
+        temp.seek(0)
 
-    r_audio = bl.get_bleeped_audio(audio,text)
-
-    r_audio.export('bleep_audio.wav', format='wav')
+        audio = AudioSegment.from_file(temp.name)
+        r_audio = bl.get_bleeped_audio(audio,text)
+        r_audio.export('bleep_audio.wav', format='wav')
 
