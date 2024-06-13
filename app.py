024c743b8d0f7599819d0372fcb0367ce5a2323f
Thethela Faltein <thethelafaltein@Thethelas-MacBook-Pro-2.local>
2024-06-13 21:06:35 +0200

    Getting the audio via file name

--- app.py
+++ app.py
@@ -11,16 +11,17 @@ uploaded_file = st.file_uploader("Choose an audio file",type = ["wav"])
 
 text = st.text_input("Enter words or phrases you want to censor/bleep:")
 
+btn = st.button("Censor")
 
-
-if uploaded_file is not None:
-    with NamedTemporaryFile(suffix=".wav") as temp:
-        temp.write(uploaded_file.getvalue())
-        temp.seek(0)
-        a = AudioSegment.from_file(file=temp.name)
-        r_audio = bl.get_bleeped_audio(uploaded_file,"Return only the  words  considered food items ",a)
-        example = r_audio.export()
-        st.audio(example.read())
-        ##r_audio.export('bleep_audio.wav', format='wav')
+if btn:
+    if uploaded_file is not None:
+        with NamedTemporaryFile(suffix=".wav") as temp:
+            temp.write(uploaded_file.getvalue())
+            temp.seek(0)
+            a = AudioSegment.from_file(file=temp.name)
+            r_audio = bl.get_bleeped_audio(uploaded_file,"Return only the  words  considered food items ",a)
+            example = r_audio.export()
+            st.audio(example.read())
+    
 
 
